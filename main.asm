fb474c0f44a0398c1e7e707dcfd0d2063373e14e
espira847 <89991317+espira847@users.noreply.github.com>
2022-02-10 12:14:52 -0500

    Add files via upload

---
+++ main.asm
@@ -0,0 +1,21 @@
+; evaluate an expression al = (al - dl) + cl - bl
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto, dwExitCode:dword
+
+.code
+main proc
+	mov al, 245
+	mov bl, 41
+	mov cl, 11
+	mov dl, 215
+	sub al, dl
+	add al, cl
+	sub al, bl
+
+	invoke ExitProcess, 0
+main endp
+end main
+
